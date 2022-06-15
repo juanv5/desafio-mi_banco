d5075b18461b09453e2a3fc1e3fabf04abb64fb0
Juan <juanvegadiaz@gmail.com>
2022-06-15 06:55:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Pool } = require('pg')
+const args = process.argv.slice(2)
+const nuevaTransaccion = require('./nuevaTransaccion')
+const consulta = require('./consulta')
+const consultarSaldo = require('./consultarSaldo')
+
+const config = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mi_banco_db',
+    password: '1234',
+    port: 5432,
+    max: 20,
+    min: 0,
+    idleTimeoutMillis: 5000,
+    connectionTimeoutMillis: 2000,
+}
+const pool = new Pool(config)
+const programa = args[0]
+
+pool.connect(async(errorConexion, client, release) => {
+    if (errorConexion) {
+        console.error(errorConexion)
+    } else {
+        if (programa === 'nuevaTransaccion') {
+            nuevaTransaccion(client, release, pool)
+
+        }
+        if (programa === 'consulta') {
+            consulta(client, release, pool)
+
+        }
+        if (programa === 'consultarSaldo') {
+            consultarSaldo(client, release, pool)
+
+        }
+    }
+})
---
+++ nuevaTransaccion.js
@@ -0,0 +1,53 @@
+const args = process.argv.slice(2)
+
+const descripcion = args[1]
+const fecha = args[2]
+const monto = args[3]
+const cuenta = args[4]
+
+async function nuevaTransaccion(client, release, pool) {
+    try {
+        const transaccion = {
+            // rowMode: 'array',
+            name: 'nuevaTransaccion',
+            text: 'INSERT INTO transacciones (descripcion, fecha, monto, cuenta) VALUES ($1, $2, $3, $4)RETURNING *;',
+            values: [descripcion, fecha, monto, cuenta]
+        }
+
+        const actualizacion = {
+            // rowMode: 'array',
+            name: 'actualizacion',
+            text: 'UPDATE transacciones SET monto = monto $3 WHERE cuenta = cuenta $4 RETURNING *;',
+            values: [monto, cuenta]
+        }
+
+        await client.query('BEGIN')
+        await client.query(transaccion, (error, res) => {
+            if (error) {
+                console.log(error.code)
+            } else {
+                console.log(res.rows)
+            }
+        })
+        await client.query(actualizacion, (error, res) => {
+            if (error) {
+                console.log(error.code)
+            } else {
+                console.log(res.rows)
+            }
+        })
+        await client.query('COMMIT')
+
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.log(`Error código: ${error.code}`)
+        console.log(`Detalle del error:${error.detail}`)
+        console.log(`Tabla originaria del error: ${error.table}`)
+        console.log(`Restricción violada en el campo: ${error.constraint}`)
+    }
+
+    release()
+    pool.end()
+}
+
+module.exports = nuevaTransaccion
